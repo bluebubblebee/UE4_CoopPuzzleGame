f945ca3c437bec819c53b7ec2f2ae005c88e89e0
bsgg <beatriz.s.g86@gmail.com>
2019-11-09 11:33:13 +0000

    Preparing project for Steam

--- CoopPuzzleGame/Source/CoopPuzzleGame/CoopPuzzleGame.Build.cs
+++ CoopPuzzleGame/Source/CoopPuzzleGame/CoopPuzzleGame.Build.cs
@@ -8,6 +8,6 @@ public class CoopPuzzleGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "OnlineSubsystem", "OnlineSubsystemSteam" });
     }
 }
